c328ddade7c6f2e82e9f3fb8c1d2f155392d2960
Leaf <wanyancao9@gmail.com>
2026-04-01 17:05:13 +0800

    update loon rules

---
+++ UpdateRules.ps1
@@ -0,0 +1,36 @@
+Push-Location $PSScriptRoot
+$domainPattern = "\s*- '\+\.(.+)'";
+$domainSuffixPattern = "\s*- '([^\+]*)'"
+$clashGfw =  
+Invoke-WebRequest -Uri https://gl.bbkss.org/https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/gfw.txt -Method Get
+| Select-Object -ExpandProperty Content
+| ForEach-Object { $_ -split "`n" }
+
+$clashDirect = 
+Invoke-WebRequest -Uri https://gl.bbkss.org/https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/direct.txt -Method Get
+| Select-Object -ExpandProperty Content
+| ForEach-Object { $_ -split "`n" }
+function  Write-LoonRules {
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string]$Path,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        $ClashRuleSet
+    )
+    $ClashRuleSet
+    | ForEach-Object {
+        if ($_ -match $domainPattern) {
+            $_ -replace $domainPattern, "DOMAIN,$1"
+        }
+        elseif ($_ -match $domainSuffixPattern) {
+            $_ -replace $domainSuffixPattern, "DOMAIN-SUFFIX,$1"
+        }
+    }
+    | Out-File $Path
+}
+Write-LoonRules -Path .\GFW_Loon -ClashRuleSet $clashGfw
+Write-LoonRules -Path .\SuperDirect_Loon -ClashRuleSet $clashDirect
+git add .
+git commit -m "update loon rules"
+git push
+
